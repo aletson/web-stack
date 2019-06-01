db995df146298b319bc052facb1e6e2b25d196f0
aletson <me@ajl.io>
2019-06-01 13:14:22 -0400

    rules named "egress" should atually, you know, enable egress

--- web-stack.tf
+++ web-stack.tf
@@ -209,7 +209,7 @@ resource "aws_security_group_rule" "ingress_http_to_alb" {
 }
 
 resource "aws_security_group_rule" "egress_alb_to_all" {
-  type = "ingress"
+  type = "egress"
   from_port = 0
   to_port = 0
   cidr_blocks = ["0.0.0.0/0"]
@@ -411,6 +411,7 @@ resource "aws_db_instance" "rds" {
   engine_version = "5.7"
   instance_class = "db.t3.micro"
   username = "webadmin"
+  skip_final_snapshot = "true"
   password = "${var.mysql_pass}"
   vpc_security_group_ids = ["${aws_security_group.rds_security_group.id}"]
   db_subnet_group_name = "${aws_db_subnet_group.rds_subnet_group.name}"
