8cbfa3552e171861340d6e5cdeae6cab05ee6ac2
aletson <me@ajl.io>
2019-06-26 23:02:47 -0400

    hec,

--- web-stack.tf
+++ web-stack.tf
@@ -447,6 +447,11 @@ resource "aws_lb_target_group" "ec2_tg" {
   protocol = "HTTP"
   vpc_id   = aws_vpc.vpc.id
 
+  health_check {
+    path = "/"
+    matcher = "200,403,404,500"
+  }
+
   stickiness {
     type            = "lb_cookie"
     cookie_duration = "86400"
